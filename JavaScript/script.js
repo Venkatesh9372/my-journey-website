8e70adb85877ae119a135f22c446e214e5ad00a3
Venkatesh Karnure <venkateshkarnure19@gmail.com>
2026-02-02 18:57:40 +0530

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,6 +4,21 @@ const body = document.body;
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark");
 }
+const titleObserver = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.6 }
+);
+
+document.querySelectorAll(".reveal-title").forEach(title => {
+  titleObserver.observe(title);
+});
+
 
 toggle.addEventListener("click", () => {
   body.classList.toggle("dark");
